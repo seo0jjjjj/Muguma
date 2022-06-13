368200015ebf6d1ec4a3a685e99acb597d4376cb
seo0j <seo0jjjjj@gmail.com>
2022-06-13 15:04:02 +0900

    메인완성 상품 상세페이지 제작

--- src/main/webapp/pages/main/Box.js
+++ src/main/webapp/pages/main/Box.js
@@ -3,39 +3,66 @@ const PI2 = Math.PI*2;
 export class Box{ // export? APP에서 imprt하려고
 
 
-    constructor(x,y){
-        
-        
-        this.index++;
+    constructor(x,y,src,id){
+       
         this.x = x; 
         this.y= y;  
-        
-        console.log(this.x,this.y);
-        /*<!--this.img = new Image();
-        this.img.src='./../../images/014.png'
+        this.src= src;  
+        this.speed= 1+Math.random()*2;
+        this.id= id;  
+        this.canDraw = false;
+
+        this.img = new Image();
+        this.img.src=src;
         this.img.addEventListener('load',()=>{
-			cosole.log("?");
-        })*/
+				this.img_width = this.img.width/5;
+				this.img_height = this.img.height/5;	
+				this.canDraw = true;	
+        })
+        
     }
 
    
     animate(ctx){
       //  ctx.save();
+        if(this.canDraw){
+	
+        this.x= this.x-this.speed*2 ;	
+    
         ctx.font = 'bold 32px sans-serif';
         ctx.fillStyle = '#111';
         ctx.beginPath();
+        
         //그리는거
-        ctx.fillRect(this.x,this.y,100,100);
+         ctx.fillStyle = '#fff';
         
-        //ctx.drawImage(this.img, this.x, this.y,300,300);
+        //그림자 설정
+        ctx.shadowColor = "gray";
+        ctx.shadowOffsetX = 8; 
+        ctx.shadowOffsetY = 8; 
+        ctx.shadowBlur = 15;
+
+		//액자
+        ctx.fillRect(this.x-5,this.y-5,this.img_width+10,this.img_height+10); // 이미지보다 살짝 큰 액자
         
+        //그림자 제거
+       	ctx.shadowColor = "";
+        ctx.shadowOffsetX = 0; 
+        ctx.shadowOffsetY = 0; 
+        ctx.shadowBlur = 0;
+        
+        //그림그리기
+        ctx.drawImage(this.img, this.x, this.y,this.img_width,this.img_height); // 이미지
+        ctx.fillText(this.id,this.x+(this.img_width/2),this.y+30); // 같이 다니는 설명
         
 
-        ctx.fillStyle = '#fff';
-        ctx.fillText(this.index,this.x+30,this.y+30);
-        this.x+=1;
+       
+        
+        
+		
         ctx.closePath();
         //ctx.restore();
+        }
     }
 
     
--- src/main/webapp/pages/main/app.js
+++ src/main/webapp/pages/main/app.js
@@ -1,56 +1,136 @@
-import {Box} from './Box.js';
-class App{
-    
-    constructor(){
-        this.canvas = document.createElement('canvas'); // canvas 태그를 만듬.
-        document.body.appendChild(this.canvas); //body에 canvas태그를 삽입.
-        this.ctx = this.canvas.getContext('2d'); // 'ctx를 사용하기위함'
-        this.boxes = [];
-        this.pixelRadio = window.devicePixelRatio > 1 ? 2 : 1 ;
+import { Box } from './Box.js';
+class App {
 
-        window.addEventListener('resize',this.resize.bind(this), false);
-        this.resize();
-        window.requestAnimationFrame(this.animate.bind(this));
-    
-        this.canvas.addEventListener('click',this.clickHandler);
-        
-        this.createBox(3);
-        
-    }
+	constructor() {
+		this.canvas = document.createElement('canvas'); // canvas 태그를 만듬.
+		document.body.appendChild(this.canvas); //body에 canvas태그를 삽입.
+		this.ctx = this.canvas.getContext('2d'); // 'ctx를 사용하기위함'
+		this.boxes = [];
+		// 화면에 보여줄 이미지 숫자
+		this.bufferSize = 7;
+		this.img_path = [ //이미지를 담는배열
+			'./../../images/001.jpg',
+			'./../../images/002.jpg',
+			'./../../images/003.jpg',
+			'./../../images/004.jpg',
+			'./../../images/005.jpg',
+			'./../../images/006.jpg',
+			'./../../images/007.jpg',
+			'./../../images/008.jpg',
+			'./../../images/009.jpg',
+			'./../../images/010.jpg',
+			'./../../images/011.jpg',
+			'./../../images/012.jpg',
+			//'./../../images/013.jpg',
+			'./../../images/014.jpg',
+			'./../../images/015.jpg',
 
-    createBox(num){
-        
-        for(let i=0; i<num; i++){
-        this.boxes.push(new Box(-100,Math.random()*this.stageHeight,this.ctx));
-    }
-    }
+		]
+
+		this.pixelRadio = window.devicePixelRatio > 1 ? 2 : 1;
+		this.image_path_iterator = 0; // iterator of image path; 
+		window.addEventListener('resize', this.resize.bind(this), false);
+		this.resize();
+		window.requestAnimationFrame(this.animate.bind(this));
+		console.log("스테이지크기는" + this.stageHeight); //화면 크기 분석
+		
+		//박스 생성
+		this.initBox(this.bufferSize);
+		//이벤트리스너 추가
+		this.canvas.addEventListener('click', (event)=> 
+		this.clickHandler(event)
+		);
+	}
+	//새로고침시 박스를 생성 (랜덤한 위치)
+	initBox(bufferSize) {
+		for (let i = 0; i < bufferSize; i++) {
+			var x = Math.random() * this.stageWidth;
+			var y = Math.random() * this.stageHeight * 0.8;
+			var src = this.img_path[i];
+			this.image_path_iterator++;
+			var id = i;
+			this.boxes.push(new Box(x, y, src, id));
+		}
+		
+
+	}
+
+	resize() {
+		this.stageWidth = document.body.clientWidth;
+		this.stageHeight = document.body.clientHeight;
+
+		this.canvas.width = this.stageWidth * this.pixelRadio;
+		this.canvas.height = this.stageHeight * this.pixelRadio;
+		this.ctx.scale(this.pixelRadio, this.pixelRadio);
+
+	}
 
 
-    resize(){
-        this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.clientHeight;
 
-        this.canvas.width = this.stageWidth * this.pixelRadio;
-        this.canvas.height = this. stageHeight * this.pixelRadio;
-        this.ctx.scale(this.pixelRadio, this.pixelRadio);
 
-    }
+	animate() {
+		window.requestAnimationFrame(this.animate.bind(this));
 
-    clickHandler(event) {
-        console.log(event);
-    }
-    
+		this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
-    animate(){
-        window.requestAnimationFrame(this.animate.bind(this));
+		//애니메이션
+		for (let i = 0; i < this.bufferSize; i++) {
 
-        this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
-        for(let i=0; i<3; i++){
-        this.boxes[i].animate(this.ctx);
-    }
-    }
+			// 이미지가 화면밖으로 나가면,
+			if (this.boxes[i].x < (-this.boxes[i].img_width)) {
+
+
+
+				// 새로운 이미지 생성
+				var x = this.stageWidth + 500; //화면밖에 생성
+				var y = Math.random() * this.stageHeight * 0.7;
+				if (this.image_path_iterator >= this.img_path.length) { //마지막 이미지 까지 노출되면
+					this.image_path_iterator = 0; // 첫번째 이미지를 가르킴.
+				}
+				var src = this.img_path[this.image_path_iterator++];
+
+				//삭제 후 재 배열
+
+				var box = new Box(x, y, src, this.image_path_iterator);
+				this.boxes.splice(i, 1,box);
+	
+				//console.log("[새로운 박스 생성]"+box.speed );
+			}
+			else if (this.boxes[i].x > (-this.boxes[i].img_width)) {
+
+				this.boxes[i].animate(this.ctx);
+			}
+
+
+		}
+	}
+//클릭시
+	clickHandler(event) {
+		let mosPosX = event.layerX;
+		let mosPosY = event.layerY;
+		let box;
+		let selectedBox;
+
+		
+		
+		//이미지 영역인지 확인
+		for (let i = 0; i<this.bufferSize; i++) {
+			box = this.boxes[i];
+			if (mosPosX > box.x && mosPosX < box.x + box.img_width && mosPosY > box.y && mosPosY < box.y + box.img_height) {
+				
+			selectedBox = box;
+			}
+			
+		}
+		if(selectedBox!=""){
+		console.log("클릭한 그림 id"+selectedBox.id);
+		location.href="./../productPage/productForm.jsp?productNum=" + selectedBox.id;
+
+		}
+		
+	}
 }
-
-window.onload = () =>{
-    new App();
+window.onload = () => {
+	new App();
+	
 }
